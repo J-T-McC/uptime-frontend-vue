4797a1097601e0ad1549a43b60ecea458d1fd1b4
J-T-McC <tyson_mcc@hotmail.com>
2020-12-10 19:40:39 -0800

    userIsAuthenticated now defaulting to local store value

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,14 +1,16 @@
-import axios from '@/helpers/axios.js'
-import store from '@/stores/state'
-import { ref, watchEffect } from 'vue'
-import { useRouter } from 'vue-router'
+import {axios, apiEndpoint, sanctumEndpoint} from '@/helpers/api.js'
 
-const userIsAuthenticated = ref(false)
+import { ref, watchEffect } from 'vue'
+import { useRouter, useRoute } from 'vue-router'
+import useLocalStore from '@/hooks/useLocalStore.js'
+
+const localStore = useLocalStore('auth');
+const userIsAuthenticated = ref(localStore.get('authenticated', false))
 
 export function useAuth () {
 
   const login = async (email, password) => {
-    return axios.post(store.state.api + '/login', {
+    return axios.post(apiEndpoint + '/login', {
       email: email,
       password: password
     }).then((response) => {
@@ -17,28 +19,35 @@ export function useAuth () {
   }
 
   const logout = async () => {
-    await axios.post(store.state.api + '/logout');
+    await axios.post(apiEndpoint + '/logout')
     userIsAuthenticated.value = false
   }
 
   const checkIfAuthenticated = async () => {
-    const response = await axios.get(store.state.api + '/authenticated');
-    userIsAuthenticated.value = response.status >= 200 && response.status < 300
+    if(!userIsAuthenticated.value) {
+      try {
+        const response = await axios.get(apiEndpoint + '/authenticated')
+        userIsAuthenticated.value = response.status >= 200 && response.status < 300
+      } catch (error) {
+        userIsAuthenticated.value = false
+      }
+    }
   }
 
   const fetchCsrf = async () => {
-    return axios.get(store.state.auth + '/sanctum/csrf-cookie')
+    return axios.get(sanctumEndpoint + '/sanctum/csrf-cookie')
   }
 
   const router = useRouter()
+  const route = useRoute()
 
   watchEffect(() => {
-    if(userIsAuthenticated.value) {
+    if (userIsAuthenticated.value && route.path === '/login') {
       router.push('/')
-    }
-    else if(!userIsAuthenticated.value) {
+    } else if (!userIsAuthenticated.value && route.path !== '/login') {
       router.push('/login')
     }
+    localStore.set('authenticated', userIsAuthenticated.value)
   })
 
   return {
