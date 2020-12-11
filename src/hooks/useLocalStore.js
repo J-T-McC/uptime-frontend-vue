f1b1faacd80628342d6af8f3c94a9b6d4bfdcba5
J-T-McC <tyson_mcc@hotmail.com>
2020-12-10 19:39:59 -0800

    Added localStore hook

---
+++ src/hooks/useLocalStore.js
@@ -0,0 +1,37 @@
+import { readonly, ref, watchEffect } from 'vue'
+
+export default function useLocalStore (localStoreKey) {
+
+  const key = readonly(localStoreKey)
+
+  let store = ref(JSON.parse(window.localStorage.getItem(key) ?? '{}'))
+
+  const get = (key, defaultValue = null) => {
+    return store.value[key] ?? defaultValue
+  }
+
+  const set = (key, value) => {
+    store.value[key] = value
+  }
+
+  const reset = () => {
+    store.value = {}
+  }
+
+  const sync = () => {
+    window.localStorage.setItem(localStoreKey, JSON.stringify(store.value))
+  }
+
+  watchEffect(() => {
+    if (store.value) {
+      sync()
+    }
+  })
+
+  return {
+    get,
+    set,
+    reset,
+  }
+
+}
