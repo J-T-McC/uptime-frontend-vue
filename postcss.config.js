d7a5df4dfb549fcbd30be6db65a293c6854bd2cb
J-T-McC <tyson_mcc@hotmail.com>
2020-12-12 23:31:41 -0800

    PostCSS & Styles
    - added nesting
    - converted css assets to postcss
    - added table and button component classes

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
 module.exports = {
-    plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
-    }
+    plugins: [
+        require('postcss-import'),
+        require('tailwindcss'),
+        require('autoprefixer'),
+        require('postcss-nesting')
+    ]
 }
