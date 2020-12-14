88c66e7164b65b3a4d67a7c3b74dc47107ccb3bd
J-T-McC <tyson_mcc@hotmail.com>
2020-12-13 20:05:04 -0800

    Added click outside directive

--- src/main.js
+++ src/main.js
@@ -1,18 +1,25 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import DashboardLayout from '@/components/layout/DashboardLayout.vue';
-import EmptyLayout from '@/components/layout/EmptyLayout.vue';
-import Modal from '@/components/interactions/Modal'
+import DashboardLayout from '@/components/layout/DashboardLayout.vue'
+import EmptyLayout from '@/components/layout/EmptyLayout.vue'
+import Modal from '@/components/interaction/Modal'
+import Toast from "vue-toastification";
+
+import "vue-toastification/dist/index.css";
 import '@/assets/styles/styles.pcss'
+
+import { ClickOutside } from '@/directives'
 
 const Vue = createApp(App)
 
-Vue.component('default-layout', DashboardLayout);
-Vue.component('empty-layout', EmptyLayout);
-
-Vue.component("modal", Modal)
+Vue.component('default-layout', DashboardLayout)
+Vue.component('empty-layout', EmptyLayout)
+Vue.component('modal', Modal)
+Vue.directive('c-outside', ClickOutside)
 
 Vue.use(router)
+Vue.use(Toast)
+
 Vue.mount('#app')
 
