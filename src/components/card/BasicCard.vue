e20992a6f0e7085479688cd8147ff38ec69bfcb0
J-T-McC <tyson_mcc@hotmail.com>
2020-12-13 20:06:19 -0800

    Added card component

---
+++ src/components/card/BasicCard.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="w-full px-6 sm:w-1/2 xl:w-1/3 mb-5">
+    <div class="flex items-center px-5 py-6 shadow-sm rounded-md bg-white relative">
+
+      <div class="absolute top-2 right-2">
+        <slot name="header"></slot>
+      </div>
+
+      <div class="p-3 rounded-full text-blue-500 bg-opacity-75">
+          <component class="h-14 w-14" :is="icon"></component>
+      </div>
+
+      <div class="mx-5">
+        <h4 class="text-2xl font-semibold text-gray-700">{{ header }}</h4>
+        <div class="text-gray-500">{{ description }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BasicCard',
+  props: {
+    icon: {
+      default: null
+    },
+    header: String,
+    description: String,
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
