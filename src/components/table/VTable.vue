f3dd31a6b4bd2b1ae3324b84e8a28ba23d9bcde7
J-T-McC <tyson_mcc@hotmail.com>
2020-12-21 17:29:45 -0800

    Updated table styles

--- src/components/table/VTable.vue
+++ src/components/table/VTable.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
     <div class="align-middle inline-block min-w-full overflow-hidden sm:rounded">
-      <table class="table table-md table-blue">
+      <table class="table table-md">
         <slot></slot>
       </table>
     </div>
