17fe2657ca0a609941f0cfcf89a9eae19e5dc58d
J-T-McC <tyson_mcc@hotmail.com>
2020-12-21 17:22:04 -0800

    Added container component

---
+++ src/components/card/Container.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex content-center w-full">
+    <div class="bg-white w-full m-3 rounded border">
+      <h2 class="p-5 border-b text-gray-900 font-semibold text-lg">
+        <slot name="header"></slot>
+      </h2>
+      <div class="p-3">
+        <slot name="body"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Container'
+}
+</script>
