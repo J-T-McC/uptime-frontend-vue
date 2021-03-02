8b3ba43c8bb4d48d31e2a93fbb5c5045d10c3e79
J-T-McC <tyson_mcc@hotmail.com>
2021-03-02 10:48:06 -0800

    Favicon and Meta

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   devServer: {
     host: 'app.laravel.test'
-  }
+  },
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      template: 'public/index.html',
+      filename: 'index.html',
+      title: 'Uptime Monitoring Service',
+    },
+  },
 }
